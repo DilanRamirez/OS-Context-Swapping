88f13e358fd9960a847ad3958c9e921727be3f52
DilanRamirez <dilanrrr@gmail.com>
2020-02-04 15:18:44 -0500

    First version of read_input.c

---
+++ read_input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+char *read_input(char input[50]){
+	printf("Enter a String: ");
+	gets(input);
+
+	return input;
+}
+
+int main(int argc, char *argv[]){
+	char input[50];
+
+	read_input(input);
+	print_input(input);
+
+	return 0;
+}
