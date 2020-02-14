a5825096c6a3daeb6e194d9e867276e0274e4975
DilanRamirez <47306089+DilanRamirez@users.noreply.github.com>
2020-02-14 14:47:04 -0700

    Update read_input.c

--- read_input.c
+++ read_input.c
@@ -1,6 +1,9 @@
+//Program that reads and imput using gets() and then print it calling another function from write_input.h
+
 #include <stdio.h>
 #include "write_input.h"
 
+//Function that reads and input using gets()
 char *read_input(char input[50]){
 	printf("Enter a String: ");
 	gets(input);
